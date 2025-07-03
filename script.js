5aae39663ab9a38a4c16b60bc05121d6043dd587
DESKTOP-OKIGVH9\Minh <minh231191@gmail.com>
2025-07-03 21:44:28 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([105.854444, 21.028511]), // Hanoi
+      zoom: 10
+    })
+  });
